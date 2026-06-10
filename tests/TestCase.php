49acf255be0c62f5986b67187d1f0f1a968b2c7f
Manuel <mmsilvano.dev@gmail.com>
2026-06-10 10:47:38 +0800

    test: isolate compiled blade cache

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\File;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $compiledViewPath = storage_path('framework/testing/views');
+
+        File::ensureDirectoryExists($compiledViewPath);
+
+        config()->set('view.compiled', $compiledViewPath);
+    }
 }
